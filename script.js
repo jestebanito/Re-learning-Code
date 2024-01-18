15bab6bc07b8b64f1e1e2f0b1ef2b7777372d3b7
jestebanito <137816026+jestebanito@users.noreply.github.com>
2024-01-17 23:00:28 -0800

    learning the fundamentals

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-'use strict';
+// Always use strict for JavaScript
+'use strict'; 
 
+// Always add semicolons to the end of a statement to indicate that it is finished
+// let variables can be dynamic, const variables are permanent 
+const myBirthYear = "1997";
 let myName = "Joshua Esteban";
-console.log(myName);
-console.log("Hello Again, " + myName);
+// Changed the let variable
 myName = "Josh";
+
+// Console logging is important to check for errors
+// How to console log a variable
+console.log(myName);
+// Example of concatenating a string with a variable
+console.log("Hello Again, " + myName);
+
+// Using uppercase constants are only for universal truths, such as these ones
+const METERS_TO_FEET = 3.281;
+const SECONDS_PER_DAY = 60*60*24;
+
+let myAge = 99;
+myAge = myAge ++;
+// console.log(myAge);
